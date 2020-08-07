bc9930f40e60af3850ab6914972361d43e3bf703
saitejdandge <saitejdandge1@gmail.com>
2020-08-08 00:50:13 +0530

    todo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DynamicProgramming
 Practise Repo
+
+TODO
+DP
+1. https://leetcode.com/problems/minimum-cost-to-merge-stones/
+2. Given set of computers, 
+    I need to choose atleast 3 computers to form a group. Each computer has its own capacity, every group total capacity formed should cross the given threshold capacity.
+    How many such max groups are possible ?
+3. 
--- matrixchain/MCM.java
+++ matrixchain/MCM.java
@@ -2,7 +2,7 @@ package matrixchain;
 
 public class MCM {
 
-    static int a[] = { 10, 20};
+    static int a[] = { 3,2,4,1};
 
     public static void main(String[] args) {
         System.out.println(solve(1, a.length - 1));
@@ -14,7 +14,7 @@ public class MCM {
             return 0;
         int ans = Integer.MAX_VALUE;
         for (int k = i; k <= j - 1; k++) {
-            int temp = solve(i, k) + solve(k + 1, j) + a[i - 1] * a[k] * a[j];
+            int temp = solve(i, k) + solve(k + 1, j) + a[i - 1] *a[k] * a[j];
             ans = Math.min(ans, temp);
         }
         return ans;
