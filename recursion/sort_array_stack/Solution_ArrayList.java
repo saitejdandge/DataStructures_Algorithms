fca81d601a8be5932022d03a8846309560038d0a
saitejdandge <saitejdandge1@gmail.com>
2020-10-14 01:11:19 +0530

    sorting stack and array list recursively

--- recursion/sort_array_stack/Solution_ArrayList.java
+++ recursion/sort_array_stack/Solution_ArrayList.java
@@ -4,35 +4,35 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
-public class Solution {
+public class Solution_ArrayList {
     public static void main(String[] args) {
 
-        Stack<Integer> list = new Stack<>();
+        ArrayList<Integer> list = new ArrayList();
 
         Random rand = new Random();
         for (int i = 0; i < 100; i++)
-            list.push(rand.nextInt(10));
+            list.add(rand.nextInt(10));
         sort(list);
         for (int i : list)
             System.out.print(i + " ");
     }
 
-    public static Stack<Integer> sort(Stack<Integer> stack) {
+    public static ArrayList<Integer> sort(ArrayList<Integer> stack) {
         if (stack.size() == 1)
             return stack;
-        int temp = stack.pop();
+        int temp = stack.remove(stack.size() - 1);
         insert(sort(stack), temp);
         return stack;
     }
 
-    private static void insert(Stack<Integer> stack, int value) {
-        if (stack.isEmpty() || stack.peek() <= value) {
-            stack.push(value);
+    private static void insert(ArrayList<Integer> stack, int value) {
+        if (stack.isEmpty() || stack.get(stack.size() - 1) <= value) {
+            stack.add(value);
             return;
         }
-        int temp = stack.pop();
+        int temp = stack.remove(stack.size() - 1);
         insert(stack, value);
-        stack.push(temp);
+        stack.add(temp);
 
     }
 }
